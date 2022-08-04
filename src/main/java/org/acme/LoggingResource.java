0a8eb74089066c3ccd782f136a8faa5d5851bc85
Jonathan Leonhardt <jonathan.arai@gmail.com>
2022-08-04 17:49:45 -0300

    atualiza endpoints de logging

--- src/main/java/org/acme/LoggingResource.java
+++ src/main/java/org/acme/LoggingResource.java
@@ -1,7 +1,11 @@
 package org.acme;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -9,38 +13,76 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 
-/* 
-/logging/com.example.mypackage
- 
-And set the log level like this:
-/logging/com.example.mypackage?level=DEBUG
-*/
+import org.hibernate.engine.jdbc.internal.Formatter;
 
 @Path("/logging")
 public class LoggingResource {
 
-   private static Level getLogLevel(Logger logger) {
-      for (Logger current = logger; current != null;) {
-         Level level = current.getLevel();
-         if (level != null)
-            return level;
-         current = current.getParent();
-      }
-      return Level.INFO;
-   }
+	private static Level getLogLevel(Logger logger) {
+		for (Logger current = logger; current != null;) {
+			Level level = current.getLevel();
+			if (level != null)
+				return level;
+			current = current.getParent();
+		}
+		return Level.INFO;
+	}
 
    @GET
    @Path("/{logger}")
    @Produces("text/plain")
-   public String logger(@PathParam("logger") String loggerName, @QueryParam("level") String level) {
-      // get the logger instance
+   public String setLogLevel(@PathParam("logger") String loggerName, @QueryParam("level") String level) {
       Logger logger = Logger.getLogger(loggerName);
-
-      // change the log-level if requested
-      if (level != null && level.length() > 0)
+      
+      if (level != null && level.length() > 0) {
          logger.setLevel(Level.parse(level));
-
-      // return the current log-level
+      }
+      
       return getLogLevel(logger).toString();
    }
+   
+   @GET
+   @Path("/{logger}")
+   @Produces("text/plain")
+	public String saveLogToFile(@PathParam("logger") String loggerName, @QueryParam("file") String fileName) {
+		Logger logger = Logger.getLogger(loggerName);
+		
+		if (fileName != null && fileName.length() > 0) {
+			try {
+				Handler handler = new FileHandler(fileName);
+				logger.addHandler(handler);
+			} catch (SecurityException | IOException e) {}
+		}
+		
+		return getLogLevel(logger).toString();
+	}
+   
+   /*
+    * mudar format
+    * ???
+    * Arrays.stream(logger.getHandlers()).forEach(
+        		 handler -> logger.removeHandler(handler));
+    * */
+   
+   @GET
+   @Path("/{logger}")
+   @Produces("text/plain")
+   public String setLogFormat(@PathParam("logger") String loggerName, @QueryParam("level") String level) {
+      Logger logger = Logger.getLogger(loggerName);
+
+      if (level != null && level.length() > 0) {
+         logger.setLevel(Level.parse(level));
+         try {
+     		Handler handler = new FileHandler("trace_handler.log", true);
+     		SimpleFormatter formatter = new SimpleFormatter();
+     		handler.setFormatter( formatter );
+     		// https://www.programcreek.com/java-api-examples/?class=java.util.logging.FileHandler&method=setFormatter
+     		// https://www.logicbig.com/tutorials/core-java-tutorial/logging/customizing-default-format.html
+     		logger.addHandler(handler);
+     	} catch (SecurityException | IOException e) {}
+      }     
+	
+      return getLogLevel(logger).toString();
+   }
+   
 }
