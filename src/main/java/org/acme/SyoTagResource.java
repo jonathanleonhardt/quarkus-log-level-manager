937f11199fd1def09fc33ebdff3c71c8760ec914
Jonathan Leonhardt <jonathan.arai@gmail.com>
2022-08-04 17:49:05 -0300

    implementa logger e log do lombok

--- src/main/java/org/acme/SyoTagResource.java
+++ src/main/java/org/acme/SyoTagResource.java
@@ -7,15 +7,30 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.jboss.logging.Logger;
+import org.jboss.logging.Logger.Level;
+
+import lombok.extern.jbosslog.JBossLog;
+
+@JBossLog
 @Path("/tag")
 public class SyoTagResource {
 	
-	//private Logger LOGGER = Logger.getLogger(SyoTagResource.class);
+	private Logger LOGGER = Logger.getLogger(SyoTagResource.class);
 
     @GET
     @Path("/list")
     @Produces(MediaType.APPLICATION_JSON)
     public List<SyoTag> hello() {
+    	log.info("log Lombok INFO /tag/list");
+    	log.debug("log Lombok DEBUG /tag/list");
+    	log.error("log Lombok ERROR /tag/list");
+    	LOGGER.info("log INFO /tag/list");
+    	LOGGER.debug("log DEBUG /tag/list");
+    	LOGGER.error("log ERRO /tag/list");
+
+    	log.log(Level.TRACE, "erro");
+    	
     	SyoTagRepository repo = new SyoTagRepository();
         return repo.listAll();
     }
